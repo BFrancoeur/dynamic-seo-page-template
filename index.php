3236a0d560ff5acc290137e6c263e9dff6ef3e82
Brian Francoeur <francoeurbj@gmail.com>
2023-04-13 20:00:44 -0400

    create template file in php with htmnl

---
+++ index.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+    <!-- add SEO metadata here -->
+    <meta name="description" content="This is a dynamic web page template that saves space, loads fast, and preserves SEO juice!" />
+    <meta name="generator" content="SEO content" />
+    <meta name="keywords" content="dynamic page, PHP, REACT, web page template">
+    <meta name="robots" content="follow, index, max-snippet:-1, max-video-preview:-1, max-image-preview: large" />
+
+    <title>Dynamic SEO Web Page</title>
+    <link rel="stylesheet" href="style.css" type="text/css">
+</head>
+
+<body>
+    <header class="page-header">
+        <div class="container">
+            <nav class="header-nav">Navigation goes here!</nav>
+        </div>
+    </header>
+    <section class="section hero">
+        <div class="container">
+            <h1 class="hero-headline">A new kind of page template that uses 5% of the space needed by a conventional web page</h1>
+            <h3 class="hero-message">Experience lightning-fast page loads with templates that make a tiny footprint.</h3>
+            <button class="btn cta-btn hero-cta-btn">Get Started</button>
+        </div>
+    </section>
+    <section class="section benefits">
+        <div class="container">
+            <div class="col col-1">
+                <div class="container info-card">
+                    <i class="icon">Icon</i>
+                    <h3 class="title">Card Title</h3>
+                    <p class="description">Description lorem ipsum dolor amet. Lorem ipsum dolor amet. Lorem ipsum dolor amet.</p>
+                </div>
+            </div>
+            <div class="col col-2">
+                <div class="container info-card">
+                    <i class="icon">Icon</i>
+                    <h3 class="title">Card Title</h3>
+                    <p class="description">Description lorem ipsum dolor amet. Lorem ipsum dolor amet. Lorem ipsum dolor amet.</p>
+                </div>
+            </div>
+            <div class="col col-3">
+                <div class="container info-card">
+                    <i class="icon">Icon</i>
+                    <h3 class="title">Card Title</h3>
+                    <p class="description">Description lorem ipsum dolor amet. Lorem ipsum dolor amet. Lorem ipsum dolor amet.</p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="section social-proof">
+        <div class="container"><!-- make this a carousel for testimonials -->
+            <div class="col col-1 testimonial">
+                <p class="name">Full Name</p>
+                <p class="testimonial">Testimonial content. Lorem ipsum dolor amet. Lorem ipsum dolor amet.</p>
+            </div>
+        </div>
+    </section>
+
+    <section class="section signup">
+        <div class="container">
+            <input type="text" placeholder="Enter your best email" />
+            <input type="button" role="submit" />
+        </div>
+    </section>
+</body>
+
+</html>
